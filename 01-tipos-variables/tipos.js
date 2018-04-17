a003622dbc4cc6ebb0f5ab8fa2adf460aa23f661
joselimaico <joselimaicotele@gmail.com>
2018-04-17 15:07:44 -0500

    variables

---
+++ 01-tipos-variables/tipos.js
@@ -0,0 +1,28 @@
+var nombre = "28"
+
+var edad = 28
+
+var fecha = new Date();
+
+var casado = false
+
+var casa = undefined
+
+var carro = null
+
+var arreglo = []
+console.log(casa?"Verdadero casa":"Falso casa")
+
+console.log(arreglo?"Verdadero":"Falso")
+if (nombre === edad) {
+    console.log("Verdadero")
+}
+else {
+    console.log("Falso")
+}
+
+console.log(-1?"Verdadero":"Falso")
+console.log(0?"Verdadero":"Falso")
+console.log(1?"Verdadero":"Falso")
+
+console.log(fecha)
